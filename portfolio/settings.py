b7bf0829b99916574c800213eb0c596c4b214da2
Josh Rubin <joshrubin2014@gmail.com>
2023-08-04 02:32:47 -0400

    got rid of postgres stuff inside settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -75,16 +75,16 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'portfoliodb',
-        'USER': 'postgres',
-        'PASSWORD': 'Pencil123$',
-        'HOST': 'localhost',
-        'PORT':'5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'portfoliodb',
+#         'USER': 'postgres',
+#         'PASSWORD': 'Pencil123$',
+#         'HOST': 'localhost',
+#         'PORT':'5432',
+#     }
+# }
 
 
 
